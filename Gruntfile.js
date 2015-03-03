52929c61852362785c9d7f9f9200de0b12390e55
Aaron Krause <aaronjkrause@gmail.com>
2015-03-02 17:47:04 -0800

    grunt basics laid out, added bower.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,9 @@
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-jscs');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-browserify');
 
   grunt.initConfig({
     jshint:{
@@ -18,9 +21,35 @@ module.exports = function(grunt) {
           src: ['*.js', 'lib/**/*.js', 'test/**/*.js']
         }
       }
+    },
+    clean:{
+      build:{
+        src:['build/']
+      }
+    }, 
+    copy:{
+      build:{
+        expand: true,
+        cwd: 'app/',
+        src: '**/*.js',
+        dest: 'build/',
+        flatten: false,
+        filter: 'isFile'
+      }
+
+    },
+    browserify:{
+      dev:{
+        src: ['app/js/**/*.js'],
+        dest: 'build/bundle.js'
+      },
+      options:{
+        transform:['debowerify']
+      }
     }
   });
 
   grunt.registerTask('default', ['jshint', 'jscs']);
+  grunt.registerTask('build', ['clean', 'browserify', 'copy'])
 
 };
