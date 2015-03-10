c0c2136cca2dada112d824d68827def900c74078
Aaron Krause <aaronjkrause@gmail.com>
2015-03-09 19:57:06 -0700

    added directives for user form and post form

--- app/js/client.js
+++ app/js/client.js
@@ -1,9 +1,14 @@
 require('angular/angular');
 
-var addPostController = require('./posts/post-controller');
-var addUserController = require('./users/user-controller');
+var addPostController = require('./posts/controllers/post-controller');
+var addUserController = require('./users/controllers/user-controller');
 
 var addResourceService = require('./services/resource-service.js');
+
+var addCreatePostDirective =
+  require('./posts/directives/create-post-directive.js');
+var addCreateUserDirective =
+  require('./users/directives/create-user-directive.js');
 
 var postApp = angular.module('postApp', []);
 
@@ -13,3 +18,7 @@ addResourceService(postApp);
 //controllers
 addPostController(postApp);
 addUserController(postApp);
+
+//directives
+addCreatePostDirective(postApp);
+addCreateUserDirective(postApp);
